9f291ecdd1fa146a8201a05374920e3ffa274ff0
ferhatkaplan <ferhatkaplan2742@gmail.com>
2022-03-08 21:57:13 +0300

    Initial commit

---
+++ src/test/java/full_stack_tekrar/FullStackTekrarApplicationTests.java
@@ -0,0 +1,13 @@
+package full_stack_tekrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackTekrarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
